2d333d654c9f9b53b64d73be8e8007c697cdfe05
nguyenlehaison <nguyenle.haison@outlook.com>
2022-08-16 22:49:13 +0700

    Update

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -6,42 +6,34 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
-    options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
-    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(options=options)
+    chromedriver = "/usr/lib/chromium-browser/chromedriver"
+    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
-    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
-    driver.find_element_by_id("login-button").click()
-    product_label = driver.find_element_by_css_selector("div[class='product_label']").text
-    assert "Products" in product_label
-    print(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
+    driver.find_element("id",'user-name').send_keys(user)
+    driver.find_element("id",'password').send_keys(password)
+    driver.find_element("id",'login-button').click()
+    print ('Login successfully')
 
-def add_cart(driver, n_items):
-    for i in range(n_items):
-        element = "a[id='item_" + str(i) + "_title_link']"  # Get the URL of the product
-        driver.find_element_by_css_selector(element).click()  # Click the URL
-        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click()  # Add the product to the cart
-        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text  # Get the name of the product from the page
-        print(timestamp() + product + " added to shopping cart.")  # Display message saying which product was added
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()  # Click the Back button
-    print(timestamp() + '{:d} items are all added to shopping cart successfully.'.format(n_items))
+    print ('Choose all items')
+    items = driver.find_elements("class name",'inventory_item')
+    for item in items:
+        name = item.find_element("class name",'inventory_item_name')
+        print ('Add ' + name.text + ' to cart')
+        item.find_element("class name",'btn_inventory').click()
+    
+    print ('Go to cart')
+    driver.find_element("class name",'shopping_cart_link').click()
+    removes = driver.find_elements("class name", "cart_item")
+    for remove in removes:
+        name = remove.find_element("class name",'inventory_item_name')
+        print ('Remove ' + name.text + ' from cart')
+        remove.find_element("class name",'cart_button').click()
 
-def remove_cart(driver, n_items):
-    for i in range(n_items):
-        element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by_css_selector(element).click()
-        driver.find_element_by_css_selector("button.btn_secondary.btn_inventory").click()
-        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text
-        print(timestamp() + product + " removed from shopping cart.")  # Display message saying which product was added
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
+    print ('Done')
 
-if __name__ == "__main__":
-    number_item = 10
-    username = 'standard_user'
-    password = 'secret_sauce'
-    driver = login( username, password )
-    add_cart(driver, N_ITEMS)
-    remove_cart(driver, N_ITEMS)
+login('standard_user', 'secret_sauce')
