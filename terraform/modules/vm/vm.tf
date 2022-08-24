7fd470090858cf3302741268844557d4865a2084
nguyenlehaison <nguyenle.haison@outlook.com>
2022-08-24 15:08:55 +0700

    Update

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,26 +1,28 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "network" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.public_ip}"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "vm-${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
   size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+  admin_username      = "hsonnl"
+  network_interface_ids = [
+    azurerm_network_interface.network.id,
+  ]
   admin_ssh_key {
-    username   = ""
-    public_key = ""
+    username   = "hsonnl"
+    public_key = file(".ssh/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
@@ -29,7 +31,7 @@ resource "azurerm_linux_virtual_machine" "" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "20_04-daily-lts-gen2"
     version   = "latest"
   }
 }
