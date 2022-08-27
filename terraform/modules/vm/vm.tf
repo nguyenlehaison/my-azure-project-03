aec87d14a4d6ab5b4fe4755f271afc4a47f28e88
nguyenlehaison <nguyenle.haison@outlook.com>
2022-08-27 18:45:22 +0700

    Update

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,8 +29,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts-gen2"
     version   = "latest"
   }
